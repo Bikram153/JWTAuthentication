393087fa4aea1d6d3c5b1b2675ea137c8303a31a
Bikram153 <samantabikramviktar@gmail.com>
2025-06-19 15:57:52 +0530

    Register endpoint made

---
+++ WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.DTO;
+using WebAPI.Infrastructure;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly DataAccess dataAccess;
+
+        public AuthController(DataAccess dataAccess)
+        {
+            this.dataAccess = dataAccess;
+        }
+
+        [HttpPost("register")]
+        public ActionResult Register(RegisterRequest request)
+        {
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(request.Password);
+            var result = dataAccess.RegisterUser(request.Email, hashedPassword, request.Role);
+            if (result)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}

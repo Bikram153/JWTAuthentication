5049555c500344983ee9e373c4ebaa2fd5a0d197
BIKRAM SAMANTA <84145819+Bikram153@users.noreply.github.com>
2025-06-20 15:18:23 +0530

    added refresh and logout endpoints (#3)

--- WebAPI/Infrastructure/DataAccess.cs
+++ WebAPI/Infrastructure/DataAccess.cs
@@ -42,5 +42,48 @@ namespace WebAPI.Infrastructure
             var sql = "select * from [USERACCOUNT] where [Email]=@email";
             return connection.QueryFirstOrDefault<UserAccount>(sql, new { email = email });
         }
+
+        public bool InsertRefreshToken(RefreshToken refreshToken, string email)
+        {
+            var sql = "insert into [RefreshToken] (Token, CreatedDate, Expires, Enabled, Email) values (@token, @createddate, @expires, @enabled, @email)";
+
+            var result = connection.Execute(sql, new
+            {
+                refreshToken.Token,
+                refreshToken.CreatedDate,
+                refreshToken.Expires,
+                refreshToken.Enabled,
+                email
+            });
+
+            return result > 0;
+        }
+
+        public bool DisableUserTokenByEmail(string email)
+        {
+            var sql = "update [RefreshToken] set [Enabled]=0 where [Email]=@email";
+            var result = connection.Execute(sql, new { email });
+            return result > 0;
+        }
+
+        public bool DisableUserToken(string token)
+        {
+            var sql = "update [RefreshToken] set [Enabled]=0 where [Token]=@token";
+            var result = connection.Execute(sql, new { token });
+            return result > 0;
+        }
+
+        public bool IsRefreshTokenValid(string token)
+        {
+            var sql = "select count(1) from RefreshToken where Token=@token and Enabled=1 and Expires>=CAST(GETDATE() AS DATE)";
+            var result = connection.ExecuteScalar<int>(sql, new { token });
+            return result > 0;
+        }
+
+        public UserAccount? FindUserByToken(string token)
+        {
+            var sql = "select UserAccount.* from RefreshToken inner join UserAccount on RefreshToken.Email=UserAccount.Email where Token=@token";
+            return connection.QueryFirstOrDefault<UserAccount>(sql, new { token });
+        }
     }
 }
